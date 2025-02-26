c21e6dcb902486f2d8c21e279bb4f2e67355dd34
JamesLimos <limosjamesfrancis@gmail.com>
2025-02-26 08:02:25 -0800

    Changed

--- COMP003A.Reflection/Program.cs
+++ COMP003A.Reflection/Program.cs
@@ -5,32 +5,26 @@
         static void Main(string[] args)
         {
             Console.WriteLine("1. What worked well for you over the last 8 weeks?: \n");
-
-            Console.WriteLine("The lecture activities allowed me to immediately apply what I learned which helped in better understanding the concepts presented. ");
+            Console.ReadLine();
 
             Console.WriteLine("\n2. What external resources did you use to aid your learning?: \n");
-
-            Console.WriteLine("Although I mostly used the resources that was provided for certain parts I used sites like stackoverflow to help me with some debugging. ");
-
+            Console.ReadLine();
+         
             Console.WriteLine("\n3. What would you change about the course?: ");
-
-            Console.WriteLine("I would make the main coding assignments be due a extra day since I've noticed I always need an extra day to finish the assignment. ");
-
+            Console.ReadLine();
+           
             Console.WriteLine("\n4. Did you learn what you expected to learn in this course?: \n");
-
-            Console.WriteLine("Yes I was expecting to learn begginer concepts of C# and I feel that i have learend the foundational concepts like data types and control structures. ");
-
+            Console.ReadLine();
+            
             Console.WriteLine("\n5. What concepts were new to you?: \n");
-
-            Console.WriteLine("Considering that I have little experience with coding, most concepts were new to me. ");
-
+            Console.ReadLine();
+            
             Console.WriteLine("\n6. What concepts have you encountered already in the past?: \n");
-
-            Console.WriteLine("Since I have little experience the only concept I have encountered in the past was the basic input and output operations.");
-
+            Console.ReadLine();
+            
             Console.WriteLine("\n7. Do you have any tips/comments for the instructor?: \n");
-
-            Console.WriteLine("I really liked how the modules were organized. It made it easier to navigate through the information.");
+            Console.ReadLine();
+            
         }
     }
 }
